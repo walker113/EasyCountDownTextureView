14730de070b3ab2811abec9192fe405e501d5085
CaMnter <yuanyu.camnter@gmail.com>
2016-03-18 14:41:50 +0800

    Update demo

--- app/src/main/java/com/camnter/easycountdowntextureview/demo/MainActivity.java
+++ app/src/main/java/com/camnter/easycountdowntextureview/demo/MainActivity.java
@@ -1,34 +1,57 @@
 package com.camnter.easycountdowntextureview.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.camnter.easycountdowntextureview.EasyCountDownTextureView;
+import com.camnter.easycountdowntextureview.demo.adapter.MainAdapter;
+import com.camnter.easyrecyclerview.holder.EasyRecyclerViewHolder;
+import com.camnter.easyrecyclerview.widget.EasyRecyclerView;
+import com.camnter.easyrecyclerview.widget.decorator.EasyDividerItemDecoration;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
-    EasyCountDownTextureView edtv;
+public class MainActivity extends AppCompatActivity {
+
+    private EasyRecyclerView mainRv;
+    private MainAdapter mainAdapter;
+    private ArrayList<Class> classes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.findViewById(R.id.style_bt).setOnClickListener(this);
-        this.edtv = (EasyCountDownTextureView) this.findViewById(R.id.edtv);
+        this.initViews();
+        this.initData();
+        this.initListeners();
     }
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.style_bt:
-                StyleActivity.startActivity(this);
-                break;
-        }
+    private void initViews() {
+        this.mainRv = (EasyRecyclerView) this.findViewById(R.id.main_rv);
+        EasyDividerItemDecoration decoration = new EasyDividerItemDecoration(this, EasyDividerItemDecoration.VERTICAL_LIST);
+        decoration.bottomDivider = true;
+        this.mainRv.addItemDecoration(decoration);
     }
+
+    private void initData() {
+        this.classes = new ArrayList<>();
+        this.classes.add(ShowActivity.class);
+        this.classes.add(StyleActivity.class);
+
+        this.mainAdapter = new MainAdapter();
+        this.mainAdapter.setList(this.classes);
+        this.mainRv.setAdapter(this.mainAdapter);
+    }
+
+    private void initListeners() {
+        this.mainAdapter.setOnItemClickListener(new EasyRecyclerViewHolder.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int i) {
+                Class c = MainActivity.this.classes.get(i);
+                MainActivity.this.startActivity(new Intent(MainActivity.this, c));
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/camnter/easycountdowntextureview/demo/StyleActivity.java
+++ app/src/main/java/com/camnter/easycountdowntextureview/demo/StyleActivity.java
@@ -1,25 +1,48 @@
 package com.camnter.easycountdowntextureview.demo;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.camnter.easycountdowntextureview.EasyCountDownTextureView;
 
 /**
  * Description：StyleActivity
  * Created by：CaMnter
  * Time：2016-03-17 17:20
  */
-public class StyleActivity extends AppCompatActivity {
+public class StyleActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static void startActivity(Context context) {
-        context.startActivity(new Intent(context, StyleActivity.class));
-    }
+    private EasyCountDownTextureView styleTv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_style);
+        this.styleTv = (EasyCountDownTextureView) this.findViewById(R.id.style_tv);
+        this.findViewById(R.id.style_hour_bt).setOnClickListener(this);
+        this.findViewById(R.id.style_minute_bt).setOnClickListener(this);
+        this.findViewById(R.id.style_second_bt).setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.style_hour_bt:
+                this.styleTv.setTimeHour(1);
+                break;
+            case R.id.style_minute_bt:
+                this.styleTv.setTimeMinute(1);
+                break;
+            case R.id.style_second_bt:
+                this.styleTv.setTimeSecond(1);
+                break;
+        }
     }
 }
